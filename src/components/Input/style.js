c92e7939e4da1a7ae4ecd110cb61d01795f21051
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-09-05 18:43:34 -0300

    :nail_care: feat: Styled Component do Input

---
+++ src/components/Input/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  width: 340px;
+  height: 30px;
+  background: #FFFFFF 0% 0% no-repeat padding-box;
+  border: 1px solid #707070;
+  padding-left: 10px;
+`
