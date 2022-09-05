25cc9f66774e3f06b521d1a26e08bcbc1e4232f9
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-09-05 18:46:07 -0300

    :sparkles: feat: Adicionando o Componente HelpSection na Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import { Header } from '../../components/Header';
+import { HelpSection } from '../../components/HelpSection';
 
 function Home() {
   return (
     <>
       <Header />
+      <HelpSection />
     </>
   );
 }
