cece112a781d84035a011ee4938271c658814c52
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-09-05 21:34:49 -0300

    :bug: fix: Deixando a propriedade height mutavel

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Button = styled.button`
   width: ${(props) => (props?.width ? `${props?.width}px` : '160px')};
-  height: 40px;
+  height: ${(props) => (props?.height ? `${props?.height}px` : '40px')};
   background: #FFFFFF 0% 0% no-repeat padding-box;
   box-shadow: 0px 1px 6px #00000029;
   border: 1px solid #707070;
