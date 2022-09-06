d7357956954091e001d1e890cabe754b7fd2908c
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-09-05 23:20:51 -0300

    :sparkles: feat: Adicionando Componente Divider e Products na Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,17 @@
 import { Header } from '../../components/Header';
 import { HelpSection } from '../../components/HelpSection';
+import { Divider } from '../../components/Divider';
+import { Products } from '../../components/Products';
+
 
 function Home() {
   return (
     <>
       <Header />
       <HelpSection />
+      <Divider>Sua seleção especial</Divider>
+      <Products />
+      <Divider>Compartilhe a novidade</Divider>
     </>
   );
 }
