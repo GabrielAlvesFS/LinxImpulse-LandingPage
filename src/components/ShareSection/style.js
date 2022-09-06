91bdc123e41e4c6de9e6e44a887fd9efe27fd813
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-09-06 01:17:10 -0300

    :nail_care: feat: Styled Component de ShareSection

---
+++ src/components/ShareSection/style.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const ShareSectionStyle = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  margin-top: 2.5rem;
+
+  p{
+    font-size: 14px;
+    text-align: center;
+    color: #888888;
+    padding: 0 10px;
+  }
+
+  .inputsContainer{
+    display: flex;
+    flex-direction: column;
+    gap: 1.2rem;
+  }
+
+  .inputsContainer div{
+    display: flex;
+    flex-direction: column;
+    gap: 0.4rem;
+  }
+
+  .btn{
+    margin-top: 1rem;
+  }
+`
