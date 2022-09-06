a529aa3ccea02abaf20168ad72d034d953697348
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-09-06 01:16:14 -0300

    :bug: fix: Deixando a propriedade width e height mutavel

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Input = styled.input`
-  width: 340px;
-  height: 30px;
+  width: ${(props) => (props?.width ? `${props?.width}px` : '340px')};
+  height: ${(props) => (props?.height ? `${props?.height}px` : '30px')};
   background: #FFFFFF 0% 0% no-repeat padding-box;
   border: 1px solid #707070;
   padding-left: 10px;
