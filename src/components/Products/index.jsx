7586db7198d46224dec451d337b4fd1088284929
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-09-07 18:32:00 -0300

    :sparkles: feat: Fazendo requisição dos produtos e mostrando eles na tela

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,98 +1,43 @@
+import { useEffect, useState } from 'react';
 import { Button } from '../Button/style';
 import { ProductCard } from '../ProductCard';
 import { ProductStyle } from './style';
 
 export function Products() {
+  const [product, setProduct] = useState([]);
+  const [page, setPage] = useState(1);
+  const url = `https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=${page}`;
+
+  const getApi = async () => {
+    const res = await fetch(url).then((data) => data.json());
+    setPage(page + 1);
+    setProduct([...product, ...res.products]);
+    console.log(product);
+  };
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
   return (
     <ProductStyle>
       <div className="box">
-        <ProductCard
-          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-          name="Nome do produto"
-          oldPrice="De: R$23,99"
-          newPrice="Por: R$19,99"
-          price="ou 2x de R$9,99"
-        />
-        <ProductCard
-          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-          name="Nome do produto"
-          oldPrice="De: R$23,99"
-          newPrice="Por: R$19,99"
-          price="ou 2x de R$9,99"
-        />
-        <ProductCard
-          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-          name="Nome do produto"
-          oldPrice="De: R$23,99"
-          newPrice="Por: R$19,99"
-          price="ou 2x de R$9,99"
-        />
-        <ProductCard
-          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-          name="Nome do produto"
-          oldPrice="De: R$23,99"
-          newPrice="Por: R$19,99"
-          price="ou 2x de R$9,99"
-        />
-        <ProductCard
-          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-          name="Nome do produto"
-          oldPrice="De: R$23,99"
-          newPrice="Por: R$19,99"
-          price="ou 2x de R$9,99"
-        />
-        <ProductCard
-          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-          name="Nome do produto"
-          oldPrice="De: R$23,99"
-          newPrice="Por: R$19,99"
-          price="ou 2x de R$9,99"
-        />
-        <ProductCard
-          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-          name="Nome do produto"
-          oldPrice="De: R$23,99"
-          newPrice="Por: R$19,99"
-          price="ou 2x de R$9,99"
-        />
-        <ProductCard
-          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-          name="Nome do produto"
-          oldPrice="De: R$23,99"
-          newPrice="Por: R$19,99"
-          price="ou 2x de R$9,99"
-        />
-        <ProductCard
-          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-          name="Nome do produto"
-          oldPrice="De: R$23,99"
-          newPrice="Por: R$19,99"
-          price="ou 2x de R$9,99"
-        />
-        <ProductCard
-          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-          name="Nome do produto"
-          oldPrice="De: R$23,99"
-          newPrice="Por: R$19,99"
-          price="ou 2x de R$9,99"
-        />
-        <ProductCard
-          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-          name="Nome do produto"
-          oldPrice="De: R$23,99"
-          newPrice="Por: R$19,99"
-          price="ou 2x de R$9,99"
-        />
-        <ProductCard
-          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-          name="Nome do produto"
-          oldPrice="De: R$23,99"
-          newPrice="Por: R$19,99"
-          price="ou 2x de R$9,99"
-        />
+        {
+          product.map((prod) => (
+            <ProductCard
+              key={prod.id}
+              image={prod.image}
+              name={prod.name}
+              desc={prod.description}
+              oldPrice={`De: R$${prod.oldPrice},00`}
+              newPrice={`Por: R$${prod.price},00`}
+              price={`ou ${prod.installments.count}x de R$${prod.installments.value}0`}
+            />
+          ))
+        }
       </div>
 
-      <Button width={260}>Ainda mais produtos aqui!</Button>
+      <Button width={260} onClick={getApi}>Ainda mais produtos aqui!</Button>
     </ProductStyle>
   );
 }
