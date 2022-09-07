26baa43b1adc9f1a5964014905e7eda2f3bf61b8
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-09-07 12:58:49 -0300

    :recycle: refactor: Formatando o arquivo

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,19 +1,21 @@
-import { Button } from "../Button/style"
-import { Card } from "./style"
+import { Button } from '../Button/style';
+import { Card } from './style';
 
-export function ProductCard(props) {
-  return(
+export function ProductCard({
+  image, name, oldPrice, newPrice, price,
+}) {
+  return (
     <Card>
       <div className="imageContainer">
-        <img src={props.image} alt="" />
+        <img src={image} alt="" />
       </div>
       <div className="infoContainer">
-        <p className="productName">{props.name}</p>
-        <span className="oldPrice">{props.oldPrice}</span>
-        <p className="newPrice">{props.newPrice}</p>
-        <span className="price">{props.price}</span>
+        <p className="productName">{name}</p>
+        <span className="oldPrice">{oldPrice}</span>
+        <p className="newPrice">{newPrice}</p>
+        <span className="price">{price}</span>
         <Button width={200} height={30}> Comprar </Button>
       </div>
     </Card>
-  )
+  );
 }
