4efa013397ed95f6aea4755720080f0d670f76fe
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-09-07 15:00:16 -0300

    :iphone: feat: Responsividade de Products para Tablet's

--- src/components/Products/style.js
+++ src/components/Products/style.js
@@ -1,8 +1,26 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { device } from '../../style/device';
 
 export const ProductStyle = styled.main`
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 3rem;
-`
+
+  .box{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3rem;
+  }
+
+  @media ${device.tablet} {
+    .box{
+      width: 100%;
+      display: grid;
+      grid-template-columns: repeat(2, 200px);
+      justify-content: center;
+    }
+  }
+`;
