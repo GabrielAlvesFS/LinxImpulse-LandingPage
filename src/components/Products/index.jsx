6ca5e8a065d0cdf0e6ab09a76a1efbe6d9af909b
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-09-07 12:21:27 -0300

    :recycle: refactor: Formatando o arquivo

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,38 +1,42 @@
-import { Button } from "../Button/style"
-import { ProductCard } from "../ProductCard"
-import { ProductStyle } from "./style"
+import { Button } from '../Button/style';
+import { ProductCard } from '../ProductCard';
+import { ProductStyle } from './style';
 
 export function Products() {
-  return(
+  return (
     <ProductStyle>
       <>
-        <ProductCard image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-        name="Nome do produto"
-        oldPrice="De: R$23,99"
-        newPrice="Por: R$19,99"
-        price="ou 2x de R$9,99"
+        <ProductCard
+          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
+          name="Nome do produto"
+          oldPrice="De: R$23,99"
+          newPrice="Por: R$19,99"
+          price="ou 2x de R$9,99"
         />
-        <ProductCard image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-        name="Nome do produto"
-        oldPrice="De: R$23,99"
-        newPrice="Por: R$19,99"
-        price="ou 2x de R$9,99"
+        <ProductCard
+          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
+          name="Nome do produto"
+          oldPrice="De: R$23,99"
+          newPrice="Por: R$19,99"
+          price="ou 2x de R$9,99"
         />
-        <ProductCard image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-        name="Nome do produto"
-        oldPrice="De: R$23,99"
-        newPrice="Por: R$19,99"
-        price="ou 2x de R$9,99"
+        <ProductCard
+          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
+          name="Nome do produto"
+          oldPrice="De: R$23,99"
+          newPrice="Por: R$19,99"
+          price="ou 2x de R$9,99"
         />
-        <ProductCard image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
-        name="Nome do produto"
-        oldPrice="De: R$23,99"
-        newPrice="Por: R$19,99"
-        price="ou 2x de R$9,99"
+        <ProductCard
+          image="https://miro.medium.com/max/960/0*i1v1In2Tn4Stnwnl.jpg"
+          name="Nome do produto"
+          oldPrice="De: R$23,99"
+          newPrice="Por: R$19,99"
+          price="ou 2x de R$9,99"
         />
       </>
 
       <Button width={260}>Ainda mais produtos aqui!</Button>
     </ProductStyle>
-  )
+  );
 }
